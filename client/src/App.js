09332e4c1ad1d7db03bf4940ed85a7c2d703ae40
Sabrina Henrice <sabrinahenrice@gmail.com>
2022-06-07 21:44:01 -0400

    material-ui not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <Hello World></Hello>
-    </div>
-  );
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+// import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline/>
+            <Header/>
+            <Grid container spacing={3} style={{ width: "100%" }}>
+                <Grid item xs={12} md={4}>
+                    <List/>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map/>
+                </Grid>
+            </Grid>
+        </>
+    )
 }
 
 export default App;
